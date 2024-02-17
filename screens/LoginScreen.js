c58263fceaf2f90652aa814288417278cd6616ec
sainath <sainathpoojari19@gmail.com>
2024-02-17 00:06:17 -0800

    login and signup validation

---
+++ screens/LoginScreen.js
@@ -0,0 +1,84 @@
+import { View, Text, Image } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { TextInput } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import Animated,{ FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native'
+
+
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [emailVerify, setEmailVerify] = useState(false)
+  const [password, setPassword] = useState('')
+  const [passwordVerify, setPasswordVerify] = useState(false)
+  const navigation = useNavigation();
+
+  function handleEmail(e) {
+    const emailVar = e.nativeEvent.text
+    setEmail(emailVar)
+    setEmailVerify(false)
+    if (/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(emailVar)) {
+      setEmail(emailVar)
+      setEmailVerify(true)
+    }
+  }
+
+  function handlePassword(e) {
+    const passwordVar = e.nativeEvent.text
+    setPassword(passwordVar)
+    setPasswordVerify(false)
+    if (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(passwordVar)) {
+      setPassword(passwordVar)
+    setPasswordVerify(true)
+    }
+  }
+
+
+  return (
+    <View className="bg-white h-full w-full">
+      <StatusBar style='light' />
+      <Image className="h-full w-full absolute" source={require('../assets/images/6.png')} />
+      {/* <View className="flex-row justify-around w-full absolute">
+        </View> */}
+{/* title and form */}
+      <View className="h-full w-full flex justify-around  pb-25">
+        {/* title*/}
+        <View className="flex items-center mx-4 space-y-4 ">
+          <Animated.View entering={FadeInDown.duration(1000).springify()}className="flex items-center">
+            <Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider text-5xl">
+              Login
+            </Text>
+          </Animated.View>
+
+          {/* form */}
+          <Animated.View entering={FadeInDown.delay(1200).duration(1000).springify()} className="bg-white p-5 rounded-2xl w-full mb-3">
+            <TextInput placeholder="EMAIL" placeholderTextColor={'crimson'} onChange={e => handleEmail(e) }/>
+          </Animated.View>
+          {email.length < 1 ? null : emailVerify ? null : (<Text className=" text-rose-700">Enter Vaild email</Text>)}
+          <Animated.View entering={FadeInDown.delay(1400).duration(1000).springify()} className="bg-white p-5 rounded-2xl w-full mb-3">
+            <TextInput placeholder="PASSWORD" placeholderTextColor={'crimson'} onChange={e=>handlePassword(e)} />
+          </Animated.View>
+          {password.length < 1 ? null : passwordVerify ? null : (<Text className=" text-rose-700">Enter Vaild password</Text>)}
+
+          <Animated.View entering={FadeInDown.delay(1600).duration(1000).springify()} className="w-full">
+            <TouchableOpacity className=" w-full bg-rose-700 p-3 rounded-2xl mb-3 ">
+              <Text onPress={()=>navigation.navigate('Home')} className=" text-xl font-bold text-white text-center">Login</Text>
+            </TouchableOpacity>
+          </Animated.View>
+         
+         <Animated.View entering={FadeInDown.delay(1800).duration(1000).springify()}  className=" flex-row justify-center">
+          <Text>Don't have an account? </Text>
+          <TouchableOpacity onPress={()=>navigation.navigate('Signup')}>
+            <Text className="text-rose-700">SignUp</Text>
+          </TouchableOpacity>
+
+         </Animated.View>
+
+
+        </View>
+      </View>
+    </View>
+  )
+}
